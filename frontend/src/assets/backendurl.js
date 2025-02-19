46d0c5958776c1a850270aff7108d72f8c788059
amitak2002 <amitkumaryadav844866@gmail.com>
2025-02-19 11:46:19 +0530

    first commit

--- frontend/src/assets/backendurl.js
+++ frontend/src/assets/backendurl.js
@@ -1,2 +1,2 @@
 //export const BACKEND_URL = 'https://course-selling-app-7usj.onrender.com'
-export const BACKEND_URL = 'https:http://localhost:2002/api/v1'
+export const BACKEND_URL = 'http://localhost:2002/api/v1'
